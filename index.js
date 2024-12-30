5afb0c25eda2c5fb3a7204df843d017ae39dfca5
prabhu797 <prabhudesai007@gmail.com>
2024-12-30 13:02:20 +0530

    fix: kill process add

--- index.js
+++ index.js
@@ -102,7 +102,8 @@ io.on('connection', (socket) => {
     });
 });
 
-const PORT = process.env.PORT || 4040;
+// const PORT = process.env.PORT || 4040;
+const PORT = 4040;
 server.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
--- start.sh
+++ start.sh
@@ -4,7 +4,19 @@ USER=$(whoami)
 
 NODE_APP_PATH="/home/$USER/frappe-bench/apps/ai_chat_assist/ai_chat_assist/node_file_backend"
 
+# Find and kill any process using port 4040
+PORT=4040
+PID=$(sudo lsof -t -i :$PORT)
 
+if [ ! -z "$PID" ]; then
+    echo "Killing process $PID using port $PORT..."
+    sudo kill -9 $PID
+else
+    echo "No process is using port $PORT."
+fi
+
+# Change to the application directory
 cd $NODE_APP_PATH
 
-node index.js
+# Start the Node.js application
+sudo node index.js
